43a9fef821d36c22e6aef8f34962f6d4cfb70b70
Horizon719 <milichovszky.sandor@gmail.com>
2023-03-10 10:41:02 +0100

    képgaléria, léptetéssel és kiskép kattintás event

--- main.js
+++ main.js
@@ -6,14 +6,32 @@ function main() {
     , "kepek/integet.jpg", "kepek/maci.jpg", "kepek/medve.jpg", "kepek/taj.jpg"];
     const article = document.querySelector("article");
     let i;
-    for (i = 0; i < 4; i++) {
+    for (i = 0; i < lista.length; i++) {
         article.innerHTML += `<div><img src="${lista[i]}" alt=""></div>`;
     }
-    for (i = 4; i < lista.length; i++) {
-        article.innerHTML += `<div><img src="${lista[i]}" alt=""></div>`;
-    }
-    const kep = document.querySelector("article div img");
-    kep.addEventListener("click", function(){
-       console.log(kep.src);
+    let currentIndex = 0;
+    const nagykep = document.querySelectorAll(".nagykep img")[0];
+    const jobb = document.getElementsByClassName("jobb")[0];
+    jobb.addEventListener("click", function(){
+        currentIndex++;
+        if (currentIndex > lista.length-1) {
+            currentIndex = 0;
+        }
+        nagykep.src = lista[currentIndex];
     })
+    const bal = document.getElementsByClassName("bal")[0];
+    bal.addEventListener("click", function(){
+        currentIndex--;
+        if (currentIndex < 0) {
+            currentIndex = lista.length-1;
+        }
+        nagykep.src = lista[currentIndex];
+    })
+    const kepek = document.querySelectorAll("article div img");
+    for (let j = 0; j < kepek.length; j++) {
+        kepek[j].addEventListener("click", function(){
+            nagykep.src = this.src;
+            currentIndex = j;    
+        })
+    }
 }
